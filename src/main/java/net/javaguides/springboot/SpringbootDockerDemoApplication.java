adad6ef7155edfe0765293204fc94c2b7ad513c0
AKSri20 <123760036+AKSri20@users.noreply.github.com>
2026-09-13 23:55:24 -0400

    Add test print statement in main application
    Added a print statement for testing purposes.

--- src/main/java/net/javaguides/springboot/SpringbootDockerDemoApplication.java
+++ src/main/java/net/javaguides/springboot/SpringbootDockerDemoApplication.java
@@ -8,6 +8,8 @@ public class SpringbootDockerDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDockerDemoApplication.class, args);
+		//Create a new branch and modift this file
+		System.out.println("Added for testing purpose");
 	}
 
 }
